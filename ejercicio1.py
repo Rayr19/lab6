2f0efac102e35c527b2fca6f9b15fd7686a836a6
Rayr19 <rubenalonsoyr@gmail.com>
2026-04-27 10:26:56 -0500

    V3: Validacion de temperatura implementada

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,18 @@
 limite_temperatura = 50
 
+def validar_temperatura(temp):
+    if temp < 0:
+        print("Error: temperatura invalida")
+        return False
+    return True
+
 def mostrar_estado(temp):
     if  temp > limite_temperatura:
-        print("Alerta: temperatura alta")
+        print("Alerta")
     else:
-        print("Estado normal")
+        print("Normal")
 
 temperatura = float(input("Ingrese temperatura: "))
-mostrar_estado(temperatura)
+
+if validar_temperatura(temperatura):
+    mostrar_estado(temperatura)
